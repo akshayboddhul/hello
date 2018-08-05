00dc1cc02c9615555122da3065227f9d94dca1d7
Akshay Blog WebApp <akshayboddhul45@gmail.com>
2018-08-05 14:57:02 +0530

    added to githubpages

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+       <h1>PARTH VENKATESH RACHCHA</h1>
+       <h5>SOLAPUR-6</h5>
+      </div>
+    );
+  }
+}
+
+export default App;
